0c941a2de555796ade18d9099aefdc56145a0826
RomaLetodiani <roman.letodiani03@gmail.com>
2023-12-04 09:09:07 +0400

    finish

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import { useDarkMode } from './contexts/DarkModeContext';
+import Main from './components/Main/Main';
+import Details from './components/details/Details';
+
 const App = () => {
+  const { bgColor } = useDarkMode();
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={`${bgColor} min-h-screen min-w-[280px]`}>
+      <Header />
+      <Routes>
+        <Route path="/">
+          <Route index element={<Main />} />
+          <Route path="/:name" element={<Details />} />
+        </Route>
+      </Routes>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { DarkModeProvider } from './contexts/DarkModeContext.tsx';
+import { HashRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <DarkModeProvider>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </DarkModeProvider>
+  </React.StrictMode>
+);
